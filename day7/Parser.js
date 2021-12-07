ab42c6310d035c5ecfa8f00351c8280549389ada
Sybren Dijkstra <nerbiz@gmail.com>
2021-12-07 17:43:53 +0100

    Solved 7-1

---
+++ day7/Parser.js
@@ -0,0 +1,12 @@
+export default class
+{
+    /**
+     * Parse lines into data
+     * @param {array} linesArray An array of lines from a file
+     * @returns {array}
+     */
+    parse(linesArray)
+    {
+        return linesArray[0].split(',').map(number => parseInt(number, 10));
+    }
+}
