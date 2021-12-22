8825151c9da8380ab615c39969c6a9bca3fa60b0
Sybren Dijkstra <nerbiz@gmail.com>
2021-12-22 19:44:09 +0100

    Parked day 15 and 19 for now

---
+++ day19-parked/Parser.js
@@ -0,0 +1,37 @@
+import Utilities from '../Utilities.js';
+
+export default class
+{
+    /**
+     * Parse lines into data
+     * @param {array} linesArray An array of lines from a file
+     * @returns {object}
+     */
+    parse(linesArray)
+    {
+        const scanners = [];
+        let scannerIndex = 0;
+
+        // Filter out the '--- scanner 0 ---' lines
+        linesArray = linesArray.filter(line => ! line.startsWith('---'));
+
+        while (linesArray.length > 0) {
+            // Add the lines up to the first empty line to a scanner
+            scanners.push({
+                index: scannerIndex++,
+                beacons: linesArray.splice(0, linesArray.indexOf(''))
+                    .map((coordinates, index) => ({
+                        index,
+                        coordinates: coordinates.split(',').map(number => parseInt(number, 10)),
+                        // Distances between beacons, to be filled in later
+                        distances: [],
+                    })),
+            });
+
+            // Remove the empty line before the next coordinate lines
+            linesArray.shift();
+        }
+
+        return scanners;
+    }
+}
