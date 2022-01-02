30b58877399b32eca6139d1487288ed1ba9ea1d5
Sybren Dijkstra <nerbiz@gmail.com>
2022-01-02 18:55:27 +0100

    19-1 works with sample data, not with puzzle data

--- day19/Parser.js
+++ day19/Parser.js
@@ -8,18 +8,25 @@ export default class
     parse(linesArray)
     {
         const scanners = [];
-        let scannerIndex = 0;
+        let scannerIndex = -1;
 
         // Filter out the '--- scanner 0 ---' lines
         linesArray = linesArray.filter(line => ! line.startsWith('---'));
 
         while (linesArray.length > 0) {
+            scannerIndex++;
+
             // Add the lines up to the first empty line to a scanner
             scanners.push({
-                index: scannerIndex++,
+                index: scannerIndex,
+                // Coordinates relative to scanner 0, to be filled in later
+                coordinates: {},
+                // Beacons that other scanners also see, to be filled in later
+                overlapping: [],
                 beacons: linesArray.splice(0, linesArray.indexOf(''))
                     .map((coordinates, index) => ({
                         index,
+                        scanner: scannerIndex,
                         coordinates: coordinates.split(',').map(number => parseInt(number, 10)),
                         // Distances between beacons, to be filled in later
                         distances: [],
