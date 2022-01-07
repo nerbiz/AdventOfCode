0a2feb7ba3a38689fda4083a7c2287d240ca8daa
Sybren Dijkstra <nerbiz@gmail.com>
2022-01-07 21:01:28 +0100

    Solved 2015 7-1

---
+++ 2015/day7/Parser.js
@@ -0,0 +1,35 @@
+import Utilities from '../../Utilities.js';
+
+export default class
+{
+    /**
+     * Parse lines into data
+     * @param {array} linesArray An array of lines from a file
+     * @returns {object}
+     */
+    parse(linesArray)
+    {
+        // Get all the wire identifiers
+        const wireIds = linesArray.join(' ')
+            .replace(/AND|OR|LSHIFT|RSHIFT|NOT|\d+|->/g, '')
+            .replace(/([a-z]+)/g, '$1,')
+            .replaceAll(' ', '')
+            .replace(/,$/, '')
+            .split(',');
+
+        return {
+            wireIds: Utilities.arrayUnique(wireIds),
+            instructions: linesArray.map(line => {
+                const [operation, wireId] = line.split(' -> ');
+    
+                return {
+                    wireId,
+                    operation: operation.split(' '),
+                    // Resolving callback is created later
+                    resolve: undefined,
+                    isResolved: false,
+                };
+            }),
+        };
+    }
+}
