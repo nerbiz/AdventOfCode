2992c8620fa57541cbe08956a95cf5ff2cd7264f
Sybren Dijkstra <nerbiz@gmail.com>
2022-01-08 04:19:09 +0100

    Solved 2015 9-1

---
+++ 2015/day9/Parser.js
@@ -0,0 +1,19 @@
+import Utilities from '../../Utilities.js';
+
+export default class
+{
+    /**
+     * Parse lines into data
+     * @param {array} linesArray An array of lines from a file
+     * @returns {array}
+     */
+    parse(linesArray)
+    {
+        return linesArray.map(line => {
+            const [places, distance] = line.split(' = ');
+            const [from, to] = places.split(' to ');
+
+            return {from, to, distance: parseInt(distance, 10)};
+        });
+    }
+}
