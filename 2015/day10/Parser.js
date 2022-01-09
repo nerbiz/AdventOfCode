e94273ab94462528ad4edbaa9f632e0850fceefd
Sybren Dijkstra <nerbiz@gmail.com>
2022-01-09 03:08:54 +0100

    Solved 2015 10-1

---
+++ 2015/day10/Parser.js
@@ -0,0 +1,14 @@
+import Utilities from '../../Utilities.js';
+
+export default class
+{
+    /**
+     * Parse lines into data
+     * @param {array} linesArray An array of lines from a file
+     * @returns {array}
+     */
+    parse(linesArray)
+    {
+        return linesArray.at(0).split('');
+    }
+}
