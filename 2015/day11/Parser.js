21ae44c401ff19d9ad88b30bb62b87bbf4c1b311
Sybren Dijkstra <nerbiz@gmail.com>
2022-01-11 12:08:11 +0100

    Improved code in day 11 solutions

--- 2015/day11/Parser.js
+++ 2015/day11/Parser.js
@@ -1,14 +1,12 @@
-import Utilities from '../../Utilities.js';
-
 export default class
 {
     /**
      * Parse lines into data
      * @param {array} linesArray An array of lines from a file
-     * @returns {array}
+     * @returns {string}
      */
     parse(linesArray)
     {
-        return linesArray.at(0).split('');
+        return linesArray.at(0);
     }
 }
