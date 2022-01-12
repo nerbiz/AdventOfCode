9303d1fea572e281a8bf93749de3ada532632702
Sybren Dijkstra <nerbiz@gmail.com>
2022-01-12 01:03:02 +0100

    Solved 2015 14-1

---
+++ 2015/day14/Parser.js
@@ -0,0 +1,23 @@
+export default class
+{
+    /**
+     * Parse lines into data
+     * @param {array} linesArray An array of lines from a file
+     * @returns {array}
+     */
+    parse(linesArray)
+    {
+        const lineRegExp = /([^\s]+).*?(\d+).*?(\d+).*?(\d+)/;
+
+        return linesArray.map(line => {
+            const [match, name, speed, duration, rest] = line.match(lineRegExp);
+
+            return {
+                name,
+                speed: parseInt(speed, 10),
+                duration: parseInt(duration, 10),
+                rest: parseInt(rest, 10),
+            };
+        });
+    }
+}
