a4f542edae8b3adcf213a0ca3d3d2db881932672
Sybren Dijkstra <nerbiz@gmail.com>
2022-01-13 13:06:57 +0100

    Solved 2015 16-1

---
+++ 2015/day16/Parser.js
@@ -0,0 +1,33 @@
+export default class
+{
+    /**
+     * Parse lines into data
+     * @param {array} linesArray An array of lines from a file
+     * @returns {array}
+     */
+    parse(linesArray)
+    {
+        // The empty line split the report with the aunt properties
+        const emptyLineIndex = linesArray.findIndex(line => line === '');
+
+        const analysis = {};
+        linesArray.slice(0, emptyLineIndex)
+            .map(line => line.split(': '))
+            .forEach(report => analysis[report[0]] = parseInt(report[1], 10));
+
+            // Sue 1: cars: 9, akitas: 3, goldfish: 0
+        const auntSues = linesArray.slice(emptyLineIndex + 1)
+            .map(line => line.replace(/^Sue \d+: /, ''))
+            .map(line => line.split(', ')
+                .map(property => property.split(': ')))
+            .map(properties => {
+                const propertiesObject = {};
+                for (const property of properties) {
+                    propertiesObject[property[0]] = parseInt(property[1], 10);
+                }
+                return propertiesObject;
+            });
+
+        return {analysis, auntSues};
+    }
+}
