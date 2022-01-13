260da06cb12b7fe9367fff10aef98656999f566d
Sybren Dijkstra <nerbiz@gmail.com>
2022-01-13 23:48:58 +0100

    Solved 2015 17-1

---
+++ 2015/day17/Parser.js
@@ -0,0 +1,15 @@
+export default class
+{
+    /**
+     * Parse lines into data
+     * @param {array} linesArray An array of lines from a file
+     * @returns {object}
+     */
+    parse(linesArray)
+    {
+        return {
+            liters: parseInt(linesArray[0].replace(/[^\d]+/, ''), 10),
+            containers: linesArray.slice(2).map(number => parseInt(number, 10)),
+        };
+    }
+}
