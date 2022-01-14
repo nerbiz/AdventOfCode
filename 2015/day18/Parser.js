77e18f066d145250ce44cce5848030e1d979407c
Sybren Dijkstra <nerbiz@gmail.com>
2022-01-14 21:16:33 +0100

    Solved 2015 18-1

---
+++ 2015/day18/Parser.js
@@ -0,0 +1,13 @@
+export default class
+{
+    /**
+     * Parse lines into data
+     * @param {array} linesArray An array of lines from a file
+     * @returns {array}
+     */
+    parse(linesArray)
+    {
+        return linesArray.map(line =>
+            line.split('').map(light => light === '#'));
+    }
+}
