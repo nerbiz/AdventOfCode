3ebc662f3fdefc65618bb333a5aa74e18bd71ca1
Sybren Dijkstra <nerbiz@gmail.com>
2022-01-19 23:08:07 +0100

    Solved 2015 22-1

---
+++ 2015/day22/Parser.js
@@ -0,0 +1,34 @@
+export default class
+{
+    /**
+     * Parse lines into data
+     * @param {array} linesArray An array of lines from a file
+     * @returns {object}
+     */
+    parse(linesArray)
+    {
+        return {
+            spells: linesArray
+                .slice(linesArray.findIndex(line => line.startsWith('Spells')) + 1)
+                .map(line => line.match(/([^\d]+)(\d+) +(\d+) +(\d+) +(\d+) +(\d+) +(\d+) +(\d+)/))
+                .map(matches => ({
+                    name: matches[1].trim(),
+                    cost: parseInt(matches[2], 10),
+                    damage: parseInt(matches[3], 10),
+                    heal: parseInt(matches[4], 10),
+                    armor: parseInt(matches[5], 10),
+                    mana: parseInt(matches[6], 10),
+                    turns: parseInt(matches[7], 10),
+                    immediate: (matches[8] === '1'),
+                })),
+            player: {
+                hitPoints: parseInt(linesArray.at(0).replace(/[^\d]+/g, ''), 10),
+                mana: parseInt(linesArray.at(1).replace(/[^\d]+/g, ''), 10),
+            },
+            boss: {
+                hitPoints: parseInt(linesArray.at(2).replace(/[^\d]+/g, ''), 10),
+                damage: parseInt(linesArray.at(3).replace(/[^\d]+/g, ''), 10),
+            },
+        };
+    }
+}
