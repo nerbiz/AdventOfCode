c08e20ef3e677c9e465d69c65a7bd44002e7500d
Sybren Dijkstra <nerbiz@gmail.com>
2022-01-19 03:53:26 +0100

    Solved 2015 15-1

---
+++ 2015/day15/Parser.js
@@ -0,0 +1,22 @@
+export default class
+{
+    /**
+     * Parse lines into data
+     * @param {array} linesArray An array of lines from a file
+     * @returns {array}
+     */
+    parse(linesArray)
+    {
+        return linesArray.map(line => {
+            const [name, properties] = line.split(': ');
+            const ingredient = {name};
+
+            for (const property of properties.split(', ')) {
+                const [name, value] = property.split(' ');
+                ingredient[name] = parseInt(value, 10);
+            }
+
+            return ingredient;
+        });
+    }
+}
