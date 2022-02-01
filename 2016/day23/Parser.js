b828a809f9dcc563f73f5c1c13625adcb17c08d5
Sybren Dijkstra <nerbiz@gmail.com>
2022-02-01 18:48:41 +0100

    Solved 2016 23-1

---
+++ 2016/day23/Parser.js
@@ -0,0 +1,24 @@
+export default class
+{
+    /**
+     * Parse lines into data
+     * @param {array} linesArray An array of lines from a file
+     * @returns {array}
+     */
+    parse(linesArray)
+    {
+        return linesArray.map(line => {
+            const instruction = line.split(' ');
+
+            if (instruction[1].match(/\d/)) {
+                instruction[1] -= 0;
+            }
+
+            if (instruction[2] !== undefined && instruction[2].match(/\d/)) {
+                instruction[2] -= 0;
+            }
+
+            return instruction;
+        });
+    }
+}
