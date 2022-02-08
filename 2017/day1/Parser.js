519e12f76fc31fd82037e63dd3707e532ecf3779
Sybren Dijkstra <nerbiz@gmail.com>
2022-02-08 21:38:43 +0100

    Solved 2017 1-1

---
+++ 2017/day1/Parser.js
@@ -0,0 +1,12 @@
+export default class
+{
+    /**
+     * Parse lines into data
+     * @param {array} linesArray An array of lines from a file
+     * @returns {array}
+     */
+    parse(linesArray)
+    {
+        return linesArray.at(0).split('');
+    }
+}
