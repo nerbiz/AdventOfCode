0aaaa12ebf0cab8f5a0fa2fd3f51f5a4511308a7
Sybren Dijkstra <nerbiz@gmail.com>
2022-08-25 11:00:02 +0200

    Small improvements to clone script

--- clone-template.sh
+++ clone-template.sh
@@ -1,18 +1,18 @@
 # Ask for year and day values
-read -p "Enter the year: " year
-read -p "Enter the day: " day
+read -rp "Enter the year: " year
+read -rp "Enter the day: " day
 
 directory="./${year}/day${day}"
 
 # Stop if the directory already exists
-if [ -d $directory ]; then
+if [ -d "$directory" ]; then
   echo "Directory ${directory} already exists"
   exit 1
 fi
 
 # Copy the template directory
-mkdir -p $directory
-cp "./template/"* $directory
+mkdir -p "$directory"
+cp "./template/"* "$directory"
 mv "${directory}/00-1.html" "${directory}/${day}-1.html"
 
 echo "Directory ${directory} created"
