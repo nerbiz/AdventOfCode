d97c25ef32cf4c8df3b6d3a18b68a48fe7081b27
Sybren Dijkstra <nerbiz@gmail.com>
2022-09-05 22:44:14 +0200

    The intcode computer now supports waiting for input

--- 2019/IntcodeComputer.js
+++ 2019/IntcodeComputer.js
@@ -7,10 +7,22 @@ export default class IntcodeComputer
     program = [];
 
     /**
+     * The current index (location) within the program
+     * @type {number}
+     */
+    programIndex = 0;
+
+    /**
      * The current input value for the program
      * @type {array}
      */
     input = [];
+
+    /**
+     * Whether the program pauses when asking for input
+     * @type {boolean}
+     */
+    waitForInput = false;
 
     /**
      * The relative base for relative mode
@@ -33,9 +45,17 @@ export default class IntcodeComputer
     {
         this.program = program;
 
-        if (input !== null) {
+        if (input.length > 0) {
             this.setInput(...input);
         }
+    }
+
+    /**
+     * Make the program wait, when asking for input
+     * @returns {void}
+     */
+    enableWaiting() {
+        this.waitForInput = true;
     }
 
     /**
@@ -44,6 +64,14 @@ export default class IntcodeComputer
     setInput(...input)
     {
         this.input = input;
+    }
+
+    /**
+     * @param {number} input
+     */
+    addInput(input)
+    {
+        this.input.push(input);
     }
 
     /**
@@ -97,10 +125,8 @@ export default class IntcodeComputer
      */
     *start()
     {
-        let index = 0;
-
-        while (index < this.program.length) {
-            const instruction = this.program[index];
+        programLoop: while (this.programIndex < this.program.length) {
+            const instruction = this.program[this.programIndex];
             const opcode = instruction % 100;
 
             // Stop at opcode 99
@@ -112,62 +138,68 @@ export default class IntcodeComputer
             const mode1 = Math.floor(instruction % 1000 / 100);
             const mode2 = Math.floor(instruction % 10000 / 1000);
             const mode3 = Math.floor(instruction % 100000 / 10000);
-            const parameter1 = this.getValue(mode1, index + 1);
-            const parameter2 = this.getValue(mode2, index + 2);
+            const parameter1 = this.getValue(mode1, this.programIndex + 1);
+            const parameter2 = this.getValue(mode2, this.programIndex + 2);
             let targetPosition;
 
             switch (opcode) {
                 // Addition
                 case 1:
-                    targetPosition = this.getPosition(mode3, index + 3);
+                    targetPosition = this.getPosition(mode3, this.programIndex + 3);
                     this.program[targetPosition] = parameter1 + parameter2;
-                    index += 4;
+                    this.programIndex += 4;
                     break;
                 // Multiplication
                 case 2:
-                    targetPosition = this.getPosition(mode3, index + 3);
+                    targetPosition = this.getPosition(mode3, this.programIndex + 3);
                     this.program[targetPosition] = parameter1 * parameter2;
-                    index += 4;
+                    this.programIndex += 4;
                     break;
                 // Input
                 case 3:
-                    targetPosition = this.getPosition(mode1, index + 1);
-                    this.program[targetPosition] = this.input.shift();
-                    index += 2;
+                    const nextInput = this.input.shift();
+                    if (this.waitForInput === true && nextInput === undefined) {
+                        this.generator = null;
+                        break programLoop;
+                    }
+
+                    targetPosition = this.getPosition(mode1, this.programIndex + 1);
+                    this.program[targetPosition] = nextInput;
+                    this.programIndex += 2;
                     break;
                 // Output
                 case 4:
                     yield parameter1;
-                    index += 2;
+                    this.programIndex += 2;
                     break;
                 // Jump if true (non-zero)
                 case 5:
-                    index = (parameter1 !== 0)
+                    this.programIndex = (parameter1 !== 0)
                         ? parameter2
-                        : index + 3;
+                        : this.programIndex + 3;
                     break;
                 // Jump if false (zero)
                 case 6:
-                    index = (parameter1 === 0)
+                    this.programIndex = (parameter1 === 0)
                         ? parameter2
-                        : index + 3;
+                        : this.programIndex + 3;
                     break;
                 // Less than
                 case 7:
-                    targetPosition = this.getPosition(mode3, index + 3);
+                    targetPosition = this.getPosition(mode3, this.programIndex + 3);
                     this.program[targetPosition] = Number(parameter1 < parameter2);
-                    index += 4;
+                    this.programIndex += 4;
                     break;
                 // Equal
                 case 8:
-                    targetPosition = this.getPosition(mode3, index + 3);
+                    targetPosition = this.getPosition(mode3, this.programIndex + 3);
                     this.program[targetPosition] = Number(parameter1 === parameter2);
-                    index += 4;
+                    this.programIndex += 4;
                     break;
                 // Change relative base
                 case 9:
                     this.relativeBase += parameter1;
-                    index += 2;
+                    this.programIndex += 2;
                     break;
             }
         }
