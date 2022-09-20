10a9f997db3fb48ded3793e360efc4b34b789286
Sybren Dijkstra <nerbiz@gmail.com>
2022-09-20 13:04:21 +0200

    Simplified the 2019 day 22 parser

--- 2019/22/Parser.js
+++ 2019/22/Parser.js
@@ -3,20 +3,15 @@ export default class Parser
     /**
      * Parse lines into data
      * @param {array} linesArray An array of lines from a file
-     * @returns {array}
+     * @returns {object}
      */
     parse(linesArray)
     {
         return {
             deckSize: linesArray.shift().match(/\d+/) - 0,
             steps: linesArray.map(line => {
-                if (line.match('new')) {
-                    return ['new'];
-                } else if (line.match('increment')) {
-                    return ['increment', line.match(/\d+/) - 0];
-                } else {
-                    return ['cut', line.split(' ')[1] - 0];
-                }
+                const [, type, amount] = line.match(/(new|cut|increment).*? (-?\d+)?/);
+                return [type, amount - 0];
             }),
         };
     }
