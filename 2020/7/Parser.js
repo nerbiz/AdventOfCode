eb01bbe53d43eff4fdf737529fd3cb6818992ca4
Sybren Dijkstra <nerbiz@gmail.com>
2022-10-02 20:35:24 +0200

    Solved 2020 7-1

---
+++ 2020/7/Parser.js
@@ -0,0 +1,24 @@
+export default class Parser
+{
+    /**
+     * Parse lines into data
+     * @param {array} linesArray An array of lines from a file
+     * @returns {array}
+     */
+    parse(linesArray)
+    {
+        return linesArray.map(rule => {
+            const [colour, contentsString] = rule.split(' bags contain ');
+
+            const contents = [];
+            for (const matches of contentsString.matchAll(/(\d+) ([a-z]+ [a-z]+)/ig)) {
+                contents.push({
+                    colour: matches[2],
+                    amount: matches[1] - 0,
+                });
+            }
+
+            return [colour, contents];
+        });
+    }
+}
