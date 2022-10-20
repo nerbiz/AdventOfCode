9b72792a734f996719073eac926607673043a424
Sybren Dijkstra <nerbiz@gmail.com>
2022-10-20 09:47:08 +0200

    Solved 2020 20-1

---
+++ 2020/20/Parser.js
@@ -0,0 +1,22 @@
+export default class Parser
+{
+    /**
+     * Parse lines into data
+     * @param {array} linesArray An array of lines from a file
+     * @returns {array}
+     */
+    parse(linesArray)
+    {
+        const tiles = [];
+        while (linesArray.length > 0) {
+            tiles.push({
+                id: linesArray.shift().replaceAll(/\D/g, '') - 0,
+                grid: linesArray.splice(0, 10).map(row => row.split('')),
+            });
+
+            linesArray.shift();
+        }
+
+        return tiles;
+    }
+}
