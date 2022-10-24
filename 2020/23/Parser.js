827b1be6128201642c3ab1ea6b300caa3f5a2d82
Sybren Dijkstra <nerbiz@gmail.com>
2022-10-24 23:14:15 +0200

    Simpler solution for 2020 23-1

--- 2020/23/Parser.js
+++ 2020/23/Parser.js
@@ -3,10 +3,10 @@ export default class Parser
     /**
      * Parse lines into data
      * @param {array} linesArray An array of lines from a file
-     * @returns {array}
+     * @returns {string}
      */
     parse(linesArray)
     {
-        return linesArray.at(0).split('').map(number => number - 0);
+        return linesArray.at(0);
     }
 }
