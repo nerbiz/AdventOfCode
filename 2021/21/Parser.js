c32afd5becd3542a7cad879ec14684d13643846b
Sybren Dijkstra <nerbiz@gmail.com>
2022-11-03 09:18:14 +0100

    Simplified 2021 21-1, added not-working versions for 2021 21-2

--- 2021/21/Parser.js
+++ 2021/21/Parser.js
@@ -3,13 +3,13 @@ export default class Parser
     /**
      * Parse lines into data
      * @param {array} linesArray An array of lines from a file
-     * @returns {object}
+     * @returns {array}
      */
     parse(linesArray)
     {
-        return {
-            1: parseInt(linesArray.shift().at(-1), 10),
-            2: parseInt(linesArray.shift().at(-1), 10),
-        };
+        return [
+            linesArray.at(0).split(' ').at(-1) - 0,
+            linesArray.at(1).split(' ').at(-1) - 0,
+        ];
     }
 }
