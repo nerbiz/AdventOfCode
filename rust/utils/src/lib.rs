1d35e72abe25d814ed795a7ffcc1f3510c40ad44
Sybren Dijkstra <nerbiz@gmail.com>
2022-12-01 11:54:01 +0100

    Added the input_as_string function

--- rust/utils/src/lib.rs
+++ rust/utils/src/lib.rs
@@ -1,10 +1,18 @@
 use std::fs;
 
-pub fn input_as_lines(file_path: &str) -> Vec<String> {
-    let contents = fs::read_to_string(file_path)
+pub fn input_as_string(file_path: &str, trim: bool) -> String {
+    let contents: String = fs::read_to_string(file_path)
         .expect(&format!("The file '{}' needs to be readable", file_path));
 
-    contents.trim()
+    if trim {
+        return String::from(contents.trim());
+    }
+
+    contents
+}
+
+pub fn input_as_lines(file_path: &str) -> Vec<String> {
+    input_as_string(file_path, true)
         .split('\n')
         .map(|line| String::from(line))
         .collect()
