e6c3fb32b6522a759b460d068ea33681eba7ee39
Sybren Dijkstra <nerbiz@gmail.com>
2022-12-01 10:34:48 +0100

    Fixed: wrong function name in template

--- rust/_template/src/main.rs
+++ rust/_template/src/main.rs
@@ -1,8 +1,8 @@
 mod part1;
-use aoc_utils::input_lines;
+use aoc_utils::input_as_lines;
 
 fn main() {
-    let input = input_lines("20xx/x/res/input-test.txt");
+    let input = input_as_lines("20xx/x/res/input-test.txt");
 
     println!("Part 1 answer: {}", part1::solve(&input));
 }
