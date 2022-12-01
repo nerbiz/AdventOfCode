1cdd29fe062d448f03c27ef56d852985b6bd037d
Sybren Dijkstra <nerbiz@gmail.com>
2022-12-01 14:52:55 +0100

    Simpler Rust solutions for 2021 day 2

--- rust/2021/2/src/part1.rs
+++ rust/2021/2/src/part1.rs
@@ -3,14 +3,11 @@ pub fn solve(commands: &Vec<(&str, i32)>) -> i32 {
     let mut depth: i32 = 0;
 
     for &(command, amount) in commands.iter() {
-        if command == "forward" {
-            position += amount;
-        } else if command == "up" {
-            depth -= amount;
-        } else if command == "down" {
-            depth += amount;
-        } else {
-            panic!("{}", format!("Invalid command '{}', valid commands are 'forward', 'up' and 'down'", command));
+        match command {
+            "forward" => position += amount,
+            "up" => depth -= amount,
+            "down" => depth += amount,
+            _ => panic!("{}", format!("Invalid command '{}', valid commands are 'forward', 'up' and 'down'", command)),
         }
     }
 
--- rust/2021/2/src/part2.rs
+++ rust/2021/2/src/part2.rs
@@ -4,15 +4,14 @@ pub fn solve(commands: &Vec<(&str, i32)>) -> i32 {
     let mut depth: i32 = 0;
 
     for &(command, amount) in commands.iter() {
-        if command == "forward" {
-            position += amount;
-            depth += aim * amount;
-        } else if command == "up" {
-            aim -= amount;
-        } else if command == "down" {
-            aim += amount;
-        } else {
-            panic!("{}", format!("Invalid command '{}', valid commands are 'forward', 'up' and 'down'", command));
+        match command {
+            "forward" => {
+                position += amount;
+                depth += aim * amount;
+            },
+            "up" => aim -= amount,
+            "down" => aim += amount,
+            _ => panic!("{}", format!("Invalid command '{}', valid commands are 'forward', 'up' and 'down'", command)),
         }
     }
 
