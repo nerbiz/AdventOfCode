9d0b78b181a1c729da7a17c19441129c5c8141db
Sybren Dijkstra <nerbiz@gmail.com>
2022-12-21 21:12:11 +0100

    Faster solution for 2022 18-1

--- rust/2022/18/src/main.rs
+++ rust/2022/18/src/main.rs
@@ -2,14 +2,18 @@ mod part1;
 use aoc_utils::input::input_as_lines;
 
 fn main() {
-    let cubes: Vec<Vec<u32>> = input_as_lines("2022/18/res/input.txt", true)
+    let mut cubes: Vec<Vec<u32>> = input_as_lines("2022/18/res/input.txt", true)
         .iter()
         .map(|line| {
-            line.split(',')
+            let mut coordinates: Vec<u32> = line.split(',')
                 .map(|number| number.parse().unwrap())
-                .collect()
+                .collect();
+
+            // Add the available sides per cube
+            coordinates.push(6);
+            coordinates
         })
         .collect();
 
-    println!("Part 1 answer: {}", part1::solve(&cubes));
+    println!("Part 1 answer: {}", part1::solve(&mut cubes));
 }
--- rust/2022/18/src/part1.rs
+++ rust/2022/18/src/part1.rs
@@ -1,26 +1,25 @@
-pub fn solve(cubes: &Vec<Vec<u32>>) -> u32 {
+pub fn solve(cubes: &mut Vec<Vec<u32>>) -> u32 {
     let mut total = 0;
 
-    for cube in cubes.iter() {
-        // Start with 6 sides, then see how many are covered
-        let mut sides: u32 = 6;
+    for index in 0..cubes.len() {
+        let (left, right) = cubes.split_at_mut(index + 1);
 
-        for other in cubes.iter() {
-            if other == cube {
-                continue;
-            }
+        let cube: &mut Vec<u32> = left.last_mut().unwrap();
 
+        for other in right.iter_mut() {
             // If the distance is 1, it's directly connected
             let distance: u32 = cube[0].abs_diff(other[0])
                 + cube[1].abs_diff(other[1])
                 + cube[2].abs_diff(other[2]);
 
+            // Reduce the exposed sides in both cubes
             if distance == 1 {
-                sides -= 1;
+                cube[3] -= 1;
+                other[3] -= 1;
             }
         }
 
-        total += sides;
+        total += cube[3];
     }
 
     total
