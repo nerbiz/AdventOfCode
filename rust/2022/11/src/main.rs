d87795fd65ec00a02d303bed264dd17ee782e3b0
Sybren Dijkstra <nerbiz@gmail.com>
2022-12-30 12:08:23 +0100

    Simpler input parsing in 2022 day 11

--- rust/2022/11/src/main.rs
+++ rust/2022/11/src/main.rs
@@ -11,31 +11,31 @@ pub struct Monkey {
 }
 
 fn main() {
-    let mut input: Vec<String> = input_as_lines("2022/11/res/input.txt", false);
-    let mut monkeys: Vec<Monkey> = Vec::new();
+    let input: Vec<String> = input_as_lines("2022/11/res/input.txt", true);
 
-    while input.len() > 0 {
-        let lines: Vec<String> = input.drain(..=6).collect();
+    let mut monkeys: Vec<Monkey> = input
+        .split(|line| line.is_empty())
+        .map(|lines| {
+            let levels: Vec<u64> = lines[1].split(": ").last().unwrap()
+                .split(", ")
+                .map(|number| number.parse().unwrap())
+                .collect();
 
-        let levels: Vec<u64> = lines[1].split(": ").last().unwrap()
-            .split(", ")
-            .map(|number| number.parse().unwrap())
-            .collect();
+            let operation: Vec<String> = lines[2].split(" = old ").last().unwrap()
+                .split(' ')
+                .map(|part| part.to_owned())
+                .collect();
 
-        let operation: Vec<String> = lines[2].split(" = old ").last().unwrap()
-            .split(' ')
-            .map(|part| part.to_owned())
-            .collect();
+            let divider: u64 = lines[3].split(' ').last().unwrap().parse().unwrap();
 
-        let divider: u64 = lines[3].split(' ').last().unwrap().parse().unwrap();
+            let next: Vec<usize> = lines[4..=5].iter()
+                .map(|line| line.split(' ').last().unwrap().parse().unwrap())
+                .rev()
+                .collect();
 
-        let next: Vec<usize> = lines[4..=5].iter()
-            .map(|line| line.split(' ').last().unwrap().parse().unwrap())
-            .rev()
-            .collect();
-
-        monkeys.push(Monkey { levels, operation, divider, next });
-    }
+            Monkey { levels, operation, divider, next }
+        })
+        .collect();
 
     println!("Part 1 answer: {}", part1::solve(&mut monkeys.clone()));
     println!("Part 2 answer: {}", part2::solve(&mut monkeys));
