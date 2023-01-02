6658a4087e0fcfe972ed7e5a78037df9383216e9
Sybren Dijkstra <nerbiz@gmail.com>
2023-01-02 11:51:34 +0100

    2022 16-1 work in progress

---
+++ rust/2022/16/src/main.rs
@@ -0,0 +1,54 @@
+mod part1;
+use std::collections::HashMap;
+use regex::{Captures, Regex};
+use aoc_utils::input::input_as_lines;
+use aoc_utils::timing::Timing;
+
+#[derive(Debug)]
+pub struct Valve {
+    name: String,
+    flow_rate: u32,
+    tunnels: Vec<String>,
+}
+
+fn main() {
+    let input: Vec<String> = input_as_lines("2022/16/res/input-test.txt", true);
+    let timing: Timing = Timing::start();
+    let regex: Regex = Regex::new(r"([A-Z]{2}).*rate=(\d+).*valves? (.+)").unwrap();
+
+    let mut flow_rates: HashMap<String, u32> = HashMap::new();
+
+    let mut valves: Vec<Valve> = input.iter()
+        .map(|line| {
+            let matches: Captures = regex.captures(line).unwrap();
+            let name: String = matches[1].to_string();
+            let flow_rate: u32 = matches[2].parse().unwrap();
+            let tunnels: Vec<String> = matches[3].split(", ")
+                .map(|name| name.to_string())
+                .collect();
+
+            flow_rates.insert(name.clone(), flow_rate);
+
+            Valve { name, flow_rate, tunnels }
+        })
+        .collect();
+
+    // Sort the tunnels of each valve by highest flow rate
+    valves.iter_mut().for_each(|valve| {
+        valve.tunnels.sort_by(|a, b| {
+            let a: &u32 = flow_rates.get(a).unwrap();
+            let b: &u32 = flow_rates.get(b).unwrap();
+            b.cmp(a)
+        });
+    });
+
+    // Convert the vector to a hashmap
+    let valves: HashMap<String, Valve> = valves.into_iter()
+        .map(|valve| (valve.name.clone(), valve))
+        .collect();
+
+    // Problem: results are different in separate runs: 1647, 1648 and 1649
+    // Amount of loops are also different
+    println!("Part 1 answer: {}", part1::solve(&valves));
+    timing.output();
+}
---
+++ rust/2022/16/src/part1.rs
@@ -0,0 +1,119 @@
+use std::collections::{HashMap, HashSet};
+use crate::Valve;
+
+const MAX_MINUTES: u32 = 30;
+
+struct State<'a> {
+    valves: &'a HashMap<String, Valve>,
+    minutes: u32,
+    valve_name: &'a String,
+    remaining: HashSet<&'a String>,
+    history: Vec<String>,
+    pressure_released: u32,
+    max_released: u32,
+    loops: u32,
+}
+
+pub fn solve(valves: &HashMap<String, Valve>) -> u32 {
+    let mut state = State {
+        valves,
+        minutes: 0,
+        valve_name: &String::from("AA"),
+        remaining: valves.keys().collect(),
+        history: Vec::new(),
+        pressure_released: 0,
+        max_released: 0,
+        loops: 0,
+    };
+
+    traverse(&mut state);
+
+    println!("Loops: {}", state.loops);
+
+    state.max_released
+}
+
+fn traverse(mut state: &mut State) -> u32 {
+    state.loops += 1;
+
+    if state.minutes == MAX_MINUTES {
+        return state.max_released;
+    }
+
+    let valve: &Valve = state.valves.get(state.valve_name).unwrap();
+
+    // Open the valve if it's closed and has a flow rate
+    if valve.flow_rate > 0 && state.remaining.contains(&valve.name) {
+        state.remaining.remove(&valve.name);
+        state.minutes += 1;
+        let pressure_released: u32 = (MAX_MINUTES - state.minutes) * valve.flow_rate;
+        state.pressure_released += pressure_released;
+
+        state.max_released = state.max_released
+            .max(state.pressure_released)
+            .max(traverse(&mut state));
+
+        // Revert the values after the recursion
+        state.remaining.insert(&valve.name);
+        state.minutes -= 1;
+        state.pressure_released -= pressure_released;
+    }
+
+    // Stop if the current path can't make a higher value
+    if highest_estimate(&state) <= state.max_released {
+        return state.max_released;
+    }
+
+    for other in valve.tunnels.iter() {
+        // Skip if this move has already been made
+        let movement_string: &String = &format!("{}>{other}", state.valve_name);
+        if state.history.contains(movement_string) {
+            continue;
+        }
+
+        // Go to the other valve with the closed state
+        state.valve_name = other;
+        state.minutes += 1;
+        state.history.push(movement_string.to_owned());
+
+        state.max_released = state.max_released
+            .max(traverse(&mut state));
+
+        // Revert the values after the recursion
+        state.valve_name = &valve.name;
+        state.minutes -= 1;
+        state.history.pop();
+    }
+
+    state.max_released
+}
+
+/// Get the highest estimated pressure release value
+fn highest_estimate(state: &State) -> u32 {
+    let remaining_valves: Vec<&Valve> = state.valves
+        .iter()
+        .filter_map(|(_, valve)| {
+            let keep: bool = state.remaining.contains(&valve.name)
+                && valve.flow_rate > 0;
+
+            match keep {
+                true => Some(valve),
+                false => None,
+            }
+        })
+        .collect();
+
+    let mut remaining_minutes: u32 = MAX_MINUTES - state.minutes;
+    let mut total: u32 = state.pressure_released;
+
+    remaining_valves.iter()
+        .for_each(|valve| {
+            if remaining_minutes >= 2 {
+                // Subtract 2 minutes for moving and opening the valve
+                remaining_minutes -= 2;
+                total += remaining_minutes * valve.flow_rate;
+            }
+        });
+
+    total
+}
