de84be43a588e44f4c1e206b07c3be178dac86aa
Sybren Dijkstra <nerbiz@gmail.com>
2023-12-04 15:16:19 +0100

    Solved 2023 4-1

---
+++ 2023/4/Parser.js
@@ -0,0 +1,21 @@
+export default class Parser
+{
+    /**
+     * Parse lines into data
+     * @param {array} linesArray An array of lines from a file
+     * @returns {array}
+     */
+    parse(linesArray)
+    {
+        return linesArray
+            .map(line => line.split(': ')[1])
+            .map(line => line.split(' | '))
+            .map(numberSides => numberSides
+                .map(numbers => numbers
+                    .split(' ')
+                    .filter(number => number !== '')
+                    .map(number => number - 0)
+                )
+            );
+    }
+}
