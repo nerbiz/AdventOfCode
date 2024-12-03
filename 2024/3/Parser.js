eec2824661d1c2f6008fb166b3967277d1e31c09
Sybren Dijkstra <nerbiz@gmail.com>
2024-12-03 22:36:44 +0100

    Solved 2024 3-1

---
+++ 2024/3/Parser.js
@@ -0,0 +1,12 @@
+export default class Parser
+{
+    /**
+     * Parse lines into data
+     * @param {array} linesArray An array of lines from a file
+     * @returns {string}
+     */
+    parse(linesArray)
+    {
+        return linesArray.join('');
+    }
+}
